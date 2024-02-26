f7af0bce03b1620a6a16bde34960f402a45ab205
gd <detgdaac@pm.me>
2024-02-26 21:47:04 +0530

    latest commit

---
+++ CompileAndRun.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+
+i686-elf-gcc -c source/kernel.c -o binaries/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
+i686-elf-gcc -T source/linker.ld -o bin/muos.bin -ffreestanding -O2 -nostdlib binaries/boot.o binaries/kernel.o -lgcc
+
+cp bin/muos.bin $HOME/Desktop/isodir/boot
+cd $HOME/Desktop
+
+grub-mkrescue -o muos.iso isodir
+ qemu-system-i386 -cdrom muos.iso
