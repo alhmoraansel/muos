2e6a1a3d4314125f95cf365fbf945dccdcd8a1a4
alhmoraansel <detgdaac@pm.me>
2024-02-27 09:52:51 +0530

    .

--- CompileAndRun.sh
+++ CompileAndRun.sh
@@ -1,13 +1,24 @@
 #! /bin/bash
 
+echo "Compiling boot.s  ..."
+
 i686-elf-as source/boot.s -o binaries/boot.o
+
+echo "Compiling kernel..."
 
 i686-elf-gcc -c source/kernel.c -o binaries/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
+echo "Linking ..."
+
 i686-elf-gcc -T source/linker.ld -o bin/muos.bin -ffreestanding -O2 -nostdlib binaries/boot.o binaries/kernel.o -lgcc
 
+echo "Generating iso file..."
+
+cd $HOME/Desktop/isodir/boot
+rm muos.bin
+cd $HOME/Documents/RandomOS
 cp bin/muos.bin $HOME/Desktop/isodir/boot
 cd $HOME/Desktop
 
-grub-mkrescue -o muos.iso isodir
-qemu-system-i386 -cdrom muos.iso
+grub-mkrescue -o muos.iso isodir    &> /dev/null
+qemu-system-i386 -cdrom muos.iso    &> /dev/null
--- source/kernel.c
+++ source/kernel.c
@@ -43,6 +43,12 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
 
+static inline uint16_t vga_int_entry(uint16_t uc, uint8_t color)
+{
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
+
+
 size_t strlen(const char* str)
 {
 	size_t len = 0;
@@ -52,7 +58,7 @@ size_t strlen(const char* str)
 }
 
 static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 20;
+static const size_t VGA_HEIGHT = 2000;
 
 size_t terminal_row;
 size_t terminal_column;
@@ -84,9 +90,25 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
+void terminal_put_int_entryat(uint16_t c, uint8_t color, size_t x, size_t y)
+{
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_int_entry(c, color);
+}
+
 void terminal_putchar(char c)
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+	if (++terminal_column == VGA_WIDTH) {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+	}
+}
+
+void terminal_putint(uint16_t c)
+{
+	terminal_put_int_entryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
@@ -109,6 +131,10 @@ void kernel_main(void)
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
+
+    for (uint16_t i =0; i<300; i++){
+        terminal_putint(i);
+    }
 	/* Newline support is left as an exercise. */
-	terminal_writestring("TEST 123 enter a");
+	terminal_writestring("TESaT  enter a");
 }
